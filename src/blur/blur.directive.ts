0f6aff4590b3c1734988e1e12d1a2246903bcb10
Tony Juntao. Wu <wujuntao@blg.local>
2017-07-11 12:25:19 +0800

    Bug fix for auto scailing based no container element

--- src/blur/blur.directive.ts
+++ src/blur/blur.directive.ts
@@ -18,12 +18,13 @@ function blurDirective() {
       src: "@",
       stddeviation: "@"
     },
+    transclude: "element",
     template: `
-            <svg width="256" height="256" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="svgBlur">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="svgBlur" style="max-width: 100%; max-height: 100%;">
                         <filter id="svgBlurFilter">
                             <feGaussianBlur id="svgGaussianBlur" in="SourceGraphic" stdDeviation="0" />
                         </filter>
-                        <image class="background" xlink:href="{{vm.src}}" href="{{vm.src}}" x="0" y="0" height="256" width="256" class="blurred" filter="url(#svgBlurFilter)" />
+                        <image class="background" xlink:href="{{vm.src}}" href="{{vm.src}}" x="0" y="0" class="blurred" filter="url(#svgBlurFilter)" />
             </svg>`
   };
   return directive;
@@ -40,4 +41,45 @@ function blurDirective() {
   }
 }
 
-class BlurController {}
+class BlurController {
+  private width: number = 0;
+  private height: number = 0;
+
+  constructor(
+    $scope: angular.IScope,
+    private $element: JQuery,
+    $attrs: any,
+    $transclude: any,
+    private src: string,
+    private stddeviation: number = 0
+  ) {
+    $scope.$watch("vm.src", (newValue, oldValue) => {
+      if (!newValue) {
+        return;
+      }
+      const image = new Image();
+      image.src = this.src;
+      image.onload = () => {
+        this.width = image.width;
+        this.height = image.height;
+        this.updateViewBox();
+      };
+    });
+
+    // $transclude((clone: any, scope: angular.IScope) => {
+    //   clone.on("load", () => {
+    //     this.width = clone[0].width;
+    //     this.height = clone[0].height;
+    //   });
+    // });
+  }
+
+  private updateViewBox() {
+    (this.$element as any)[0].setAttribute(
+      "viewBox",
+      [0, 0, this.width, this.height].join(" ")
+    );
+  }
+}
+
+BlurController.$inject = ["$scope", "$element", "$attrs", "$transclude"];
